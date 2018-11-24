1cbfb8eb109d5e9695489a2697533a4f23352547
Edgar <edgar.gamarra21@gmail.com>
2018-11-24 18:35:22 -0500

    creacion de componentes

---
+++ src/components/WeatherData.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const WheatherData = () =>(
+    <div>
+        Wheather Data
+    </div>
+)
+
+export default WheatherData;
---
+++ src/components/WeatherLocation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+import Location from './Location';
+import WheatherData from './WeatherData'
+const WeatherLocation = () => (
+    <div>
+        WeatherLocation
+        <Location></Location>
+        <WheatherData></WheatherData>
+    </div>
+)
+
+export default WeatherLocation;
