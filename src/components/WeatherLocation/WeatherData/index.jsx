c6461cab059745c87126ab8efba89287754e8d21
Edgar Gamarra <edgar.gamarra21@gmail.com>
2018-11-28 22:54:53 -0500

    destructurin primera forma

--- src/components/WeatherLocation/WeatherData/index.jsx
+++ src/components/WeatherLocation/WeatherData/index.jsx
@@ -3,17 +3,20 @@ import React from 'react';
 import WheatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
 
-import { CLOUDY} from './../../../constants/Weather';
+
 //css
 import './styles.css';
-const WheatherData = () => (
-    <div className="weatherDataCont">
-        <WeatherTemperature
-            temperature={20}
-            weatherState={CLOUDY}
-        />
-        <WheatherExtraInfo humidity={80} wind={"10 m/s"} />
-    </div>
-)
+const WheatherData = ({ data }) => {
+    const { temperature, weatherState, humidity, wind } = data;
+    return (
+        <div className="weatherDataCont">
+            <WeatherTemperature
+                temperature={temperature}
+                weatherState={weatherState}
+            />
+            <WheatherExtraInfo humidity={humidity} wind={wind} />
+        </div>
+    )
+}
 
 export default WheatherData;
--- src/components/WeatherLocation/index.jsx
+++ src/components/WeatherLocation/index.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import Location from './Location';
 import WheatherData from './WeatherData/index';
 
+import { SUN } from './../../constants/Weather';
 import './style.css';
 
+const data = {
+    temperature: 5,
+    weatherState: SUN,
+    humidity: 10,
+    wind: "10 m/s"
+}
 const WeatherLocation = () => (
     <div>
         <div className="weatherLocationCont">
             <Location city={"Lima"}></Location>
-            <WheatherData></WheatherData>
+            <WheatherData data={data}></WheatherData>
         </div>
 
     </div>
