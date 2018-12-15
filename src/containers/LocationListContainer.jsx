55a60061b1d9f6be63bf47aaa20af18047765504
Edgar Gamarra <edgar.gamarra21@gmail.com>
2018-12-15 13:33:11 -0500

    creacion de container LocationListContainer y correccion de la sintaxis proptypes

--- src/containers/LocationListContainer.jsx
+++ src/containers/LocationListContainer.jsx
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import {setCity} from './../actions';
+import { connect } from 'react-redux';
+import { setCity } from './../actions';
 import LocationList from './../components/LocationList';
 
 class LocationListContainer extends Component {
-    printForeCast = city => {
+  printForeCast = city => {
 
-        this.props.setCity(city);
-    
-      }
-    render() {
-        return (
-            <div>
-                <LocationList
-                  cities={this.props.cities}
-                  onWeatherLocationClick={this.printForeCast}
-                ></LocationList>
-            </div>
-        );
-    }
+    this.props.setCity(city);
+
+  }
+  render() {
+    return (
+      <div>
+        <LocationList
+          cities={this.props.cities}
+          onWeatherLocationClick={this.printForeCast}
+        ></LocationList>
+      </div>
+    );
+  }
 }
 
-LocationListContainer.prototype = {
-    setCity: PropTypes.func.isRequired,
-  }
+LocationListContainer.propTypes = {
+  setCity: PropTypes.func.isRequired,
+}
 
-  const mapDispatchToProps = dispatch => (
-    {
-      setCity: value => dispatch(setCity(value))
-    }
-  )
+const mapDispatchToProps = dispatch => (
+  {
+    setCity: value => dispatch(setCity(value))
+  }
+)
 export default connect(null, mapDispatchToProps)(LocationListContainer);
